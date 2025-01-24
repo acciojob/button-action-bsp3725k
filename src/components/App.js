65748fa45ff86d587f0361ade8e7424a815bc940
bsp3725k <bhavanisivaprasad39@gmail.com>
2025-01-24 10:07:29 +0530

    Para is visible when button clicks

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,23 @@ import './../styles/App.css';
 
 const App = (props) => {
 
+  // State to manage paragraph visibility
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Function to toggle visibility state
+  const handleClick = () => {
+    setIsVisible(true);
+  }
+
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id="click" onClick={handleClick}>
+        Show Paragraph
+      </button>
+      <p id="para" className={isVisible ? "show" : "hide"}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+      </p>
     </div>
   );
 }
